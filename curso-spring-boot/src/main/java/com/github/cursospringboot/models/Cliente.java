a3df8cf8e70f0d5fdeaceaa9c153641a4d7e05d1
Paulo Santos <paulomartins_santos@hotmail.com>
2023-06-28 20:41:10 -0300

    Create Cliente.java

---
+++ curso-spring-boot/src/main/java/com/github/cursospringboot/models/Cliente.java
@@ -0,0 +1,26 @@
+package com.github.cursospringboot.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.hibernate.annotations.GenericGenerator;
+import java.util.UUID;
+
+@Entity
+@Table(name = "clientes")
+@Builder
+@AllArgsConstructor
+
+public class Cliente {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", columnDefinition = "VARCHAR(255)")
+
+    private UUID id;
+    private String nome;
+    private String email;
+
+
+}
