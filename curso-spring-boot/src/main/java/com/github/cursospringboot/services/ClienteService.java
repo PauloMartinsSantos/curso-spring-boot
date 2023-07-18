04986c16ed44ae07de32265d5f80e9dfb6427c72
Paulo Santos <paulomartins_santos@hotmail.com>
2023-07-18 12:07:32 -0300

    Update ClienteService.java

--- curso-spring-boot/src/main/java/com/github/cursospringboot/services/ClienteService.java
+++ curso-spring-boot/src/main/java/com/github/cursospringboot/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.github.cursospringboot.services;
 
+import com.github.cursospringboot.dto.ClienteDto;
 import com.github.cursospringboot.models.Cliente;
 import com.github.cursospringboot.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,5 +15,12 @@ public class ClienteService {
     private ClienteRepository repository;
     public List<Cliente> listarClientes() {
         return repository.findAll();
+    }
+    public void criar(ClienteDto dto){
+        Cliente cliente = Cliente.builder()
+                .nome(dto.getNome())
+                .email(dto.getEmail())
+                .build();
+        repository.save(cliente);
     }
 }
